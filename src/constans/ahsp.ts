db8bd9a4c07db4f030ab659c51c4e7937c38ac25
siburianbasrunki <basrunksiburian@gmail.com>
2025-08-31 13:26:19 +0700

    fix: build

--- src/constans/ahsp.ts
+++ src/constans/ahsp.ts
@@ -4,19 +4,16 @@ export const GROUP_LABEL: Record<GroupKey, "A" | "B" | "C" | "X"> = {
   LABOR: "A",
   MATERIAL: "B",
   EQUIPMENT: "C",
-  OTHER: "X",
 };
 
 export const GROUP_TITLE: Record<GroupKey, string> = {
   LABOR: "TENAGA",
   MATERIAL: "BAHAN",
   EQUIPMENT: "PERALATAN",
-  OTHER: "LAIN-LAIN",
 };
 
 export const GROUP_ORDER: GroupKey[] = [
   "LABOR",
   "MATERIAL",
   "EQUIPMENT",
-  "OTHER",
 ];
